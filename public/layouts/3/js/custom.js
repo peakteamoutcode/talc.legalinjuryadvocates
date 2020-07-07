17eba657a50efb9c1692eb30cc3ffab8bbc3da5f
Cory Stevenson <cory@lab270.com>
2020-07-07 16:58:18 -0500

    updated

--- public/layouts/3/js/custom.js
+++ public/layouts/3/js/custom.js
@@ -20,23 +20,3 @@ $("form.claim-form .slide-four button.form-btn").click(function(){
     $(".form-slide").hide();
     $(".form-slide.slide-five").show();
 });
-
-$("form.claim-form .slide-two button.form-btn.error-no").click(function(){
-    $(".form-slide.slide-two").show();
-    $(".form-slide.slide-three").hide();
-    $(this).siblings(".error-text").show();
-});
-
-
-$("form.claim-form .slide-three button.form-btn.error-no").click(function(){
-    $(".form-slide.slide-three").show();
-    $(".form-slide.slide-four").hide();
-    $(this).siblings(".error-text").show();
-});
-
-
-$("form.claim-form .slide-four button.form-btn.error-no").click(function(){
-    $(".form-slide.slide-four").show();
-    $(".form-slide.slide-five").hide();
-    $(this).siblings(".error-text").show();
-});
