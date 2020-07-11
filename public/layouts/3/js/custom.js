233cb2bfdaeacabff5fe80ef57832fd31cbbb19b
Cory Stevenson <cory@lab270.com>
2020-07-10 20:22:43 -0500

    updated

--- public/layouts/3/js/custom.js
+++ public/layouts/3/js/custom.js
@@ -1,22 +1,29 @@
 $("form.claim-form .slide-one button.form-btn").click(function(){
     $(".form-slide").hide();
     $(".form-slide.slide-two").show();
+    $('input[name="diagnosed_when"]').val(this.innerText);
 });
 
 $("form.claim-form .slide-two button.form-btn").click(function(){
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-three").show();
+    var answer = this.innerText.toLowerCase().indexOf('yes') !== -1 ? 'true' : 'false';
+    $('input[name="under_65"]').val(answer);
 });
 
 $("form.claim-form .slide-three button.form-btn").click(function(){
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-four").show();
+    var answer = this.innerText.toLowerCase().indexOf('yes') !== -1 ? 'true' : 'false';
+    $('input[name="over_4_years"]').val(answer);
 });
 
 $("form.claim-form .slide-four button.form-btn").click(function(){
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-five").show();
+    var answer = this.innerText.toLowerCase().indexOf('yes') !== -1 ? 'true' : 'false';
+    $('input[name="has_attorney"]').val(answer);
 });
