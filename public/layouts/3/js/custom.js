0b719743a93c3c9eab7db4a46c93147a78da9c79
Cory Stevenson <cory@lab270.com>
2020-07-13 08:54:57 -0500

    updated

--- public/layouts/3/js/custom.js
+++ public/layouts/3/js/custom.js
@@ -9,7 +9,7 @@ $("form.claim-form .slide-two button.form-btn").click(function(){
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-three").show();
-    $('input[name="diagnosis"]').val(this.innerText);
+    $('input[name="diagnosed"]').val(this.innerText);
 });
 
 $("form.claim-form .slide-three button.form-btn").click(function(){
@@ -17,7 +17,7 @@ $("form.claim-form .slide-three button.form-btn").click(function(){
     $(".form-slide").hide();
     $(".form-slide.slide-four").show();
     var answer = this.innerText.toLowerCase().indexOf('yes') !== -1 ? 'after 2010' : 'before 2010';
-    $('input[name="diagnosis_when"]').val(answer);
+    $('input[name="diagnosed_when"]').val(answer);
 });
 
 $("form.claim-form .slide-four button.form-btn").click(function(){
