f219b4f1a0b87d0904828467b0da9b82fcd65a46
Cory Stevenson <cory@lab270.com>
2020-07-14 18:53:51 -0500

    updated

---
+++ public/layouts/3/js/custom.js
@@ -0,0 +1,55 @@
+$("form.claim-form .slide-one button.form-btn").click(function () {
+    $(".form-slide").hide();
+    $(".form-slide.slide-two").show();
+});
+
+$("form.claim-form .slide-two button.form-btn").click(function () {
+    $("form.claim-form p.error-text").hide();
+    $(".form-slide").hide();
+    $(".form-slide.slide-three").show();
+});
+
+$("form.claim-form .slide-three button.form-btn").click(function () {
+    $("form.claim-form p.error-text").hide();
+    $(".form-slide").hide();
+    $(".form-slide.slide-four").show();
+});
+
+$("form.claim-form .slide-four button.form-btn").click(function () {
+    $("form.claim-form p.error-text").hide();
+    $(".form-slide").hide();
+    $(".form-slide.slide-five").show();
+});
+
+$("form.claim-form .slide-two button.form-btn.error-no").click(function () {
+    $(".form-slide.slide-two").show();
+    $(".form-slide.slide-three").hide();
+    $(this).siblings(".error-text").show();
+});
+
+
+$("form.claim-form .slide-three button.form-btn.error-no").click(function () {
+    $(".form-slide.slide-three").show();
+    $(".form-slide.slide-four").hide();
+    $(this).siblings(".error-text").show();
+});
+
+
+$("form.claim-form .slide-four button.form-btn.error-no").click(function () {
+    $(".form-slide.slide-four").show();
+    $(".form-slide.slide-five").hide();
+    $(this).siblings(".error-text").show();
+});
+
+
+$("#phone_cell, #phone_home").keyup(function () {
+    var firstInput = document.getElementById("phone_cell").value;
+    var secondInput = document.getElementById("phone_home").value;
+    if (firstInput != secondInput) {
+        $("#phone_cell, #phone_home").addClass("not-confirmed");
+        console.log("notmatched");
+    } else if (firstInput === secondInput) {
+        $("#phone_cell, #phone_home").removeClass("not-confirmed");
+        console.log("matched");
+    }
+});
