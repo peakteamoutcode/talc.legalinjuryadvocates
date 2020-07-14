ff147ca677c1bfeaf30f63df2a5159507db1771c
Cory Stevenson <cory@lab270.com>
2020-07-14 18:59:38 -0500

    updated

--- public/layouts/3/js/custom.js
+++ public/layouts/3/js/custom.js
@@ -1,44 +1,30 @@
 $("form.claim-form .slide-one button.form-btn").click(function () {
     $(".form-slide").hide();
     $(".form-slide.slide-two").show();
+    $('input[name="over_4_years"]').val(answer);
 });
 
 $("form.claim-form .slide-two button.form-btn").click(function () {
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-three").show();
+    $('input[name="diagnosed"]').val(this.innerText);
 });
 
 $("form.claim-form .slide-three button.form-btn").click(function () {
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-four").show();
+    var answer = this.innerText.toLowerCase().indexOf('yes') !== -1 ? 'after 2010' : 'before 2010';
+    $('input[name="diagnosed_when"]').val(answer);
 });
 
 $("form.claim-form .slide-four button.form-btn").click(function () {
     $("form.claim-form p.error-text").hide();
     $(".form-slide").hide();
     $(".form-slide.slide-five").show();
-});
-
-$("form.claim-form .slide-two button.form-btn.error-no").click(function () {
-    $(".form-slide.slide-two").show();
-    $(".form-slide.slide-three").hide();
-    $(this).siblings(".error-text").show();
-});
-
-
-$("form.claim-form .slide-three button.form-btn.error-no").click(function () {
-    $(".form-slide.slide-three").show();
-    $(".form-slide.slide-four").hide();
-    $(this).siblings(".error-text").show();
-});
-
-
-$("form.claim-form .slide-four button.form-btn.error-no").click(function () {
-    $(".form-slide.slide-four").show();
-    $(".form-slide.slide-five").hide();
-    $(this).siblings(".error-text").show();
+    var answer = this.innerText.toLowerCase().indexOf('yes') !== -1 ? 'true' : 'false';
+    $('input[name="has_attorney"]').val(answer);
 });
 
 
@@ -47,9 +33,7 @@ $("#phone_cell, #phone_home").keyup(function () {
     var secondInput = document.getElementById("phone_home").value;
     if (firstInput != secondInput) {
         $("#phone_cell, #phone_home").addClass("not-confirmed");
-        console.log("notmatched");
     } else if (firstInput === secondInput) {
         $("#phone_cell, #phone_home").removeClass("not-confirmed");
-        console.log("matched");
     }
 });
