1e635f97e99207e8d620c1123046cc0e7344dc8d
Jyoti Duwal <jduwal@outcodesoftware.com>
2022-06-16 11:18:58 +0545

    [Update] Added a logger in 3 blade to verify if the latest changes are being reflected on staging.

--- resources/views/3.blade.php
+++ resources/views/3.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Talc Lawsuits</title>
+    <title>Talc Lawsuits  (c)</title>
     <!-- CSS -->
     <link href="/layouts/3/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
