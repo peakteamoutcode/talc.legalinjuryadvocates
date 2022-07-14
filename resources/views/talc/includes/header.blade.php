5a06c45100a0ebd8b18b3580732e99a35a06bc56
peakteamoutcode <108359786+peakteamoutcode@users.noreply.github.com>
2022-07-14 02:23:34 -0500

    Update header.blade.php
    Change phone number:
    1. Update phone number on Landing page to: (800) 484-6757

--- resources/views/talc/includes/header.blade.php
+++ resources/views/talc/includes/header.blade.php
@@ -1,6 +1,6 @@
 <header>
   <div class="container-fliud">
     <div class="logo"> <a href="/"><img src="{{ asset('/img/logo.png') }}" alt="logo"></a> </div>
-    <div class="headerBtn float-end"> <a href="tel:1-866465724" class="btn btn-success">1-866-46-5724</a> </div>
+    <div class="headerBtn float-end"> <a href="tel:8004846757" data-phone="test-oc" class="btn btn-success">(800) 484-6757</a> </div>
   </div>
 </header>
